be608a3d0ead8ffe932029a70ace9f709667a254
Jacob Coccari <jacob@Jacobs-MacBook-Pro.local>
2023-11-10 20:38:13 -0700

    properly configured the gitignore

--- chunk_and_serialize.py
+++ chunk_and_serialize.py
@@ -6,8 +6,9 @@ headers_to_split_on = [
     ("#", "Header 1"),
     ("##", "Header 2"),
     ("###", "Header 3"),
+    ("####", "Header 4"),
 ]
 
 markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
 md_header_splits = markdown_splitter.split_text(markdown_document)
-md_header_splits
+print(md_header_splits)
