b8c32835bfff4b3e2625fa359116169c6d72e32c
Jacob Coccari <jacob@Jacobs-MacBook-Pro.local>
2023-11-12 15:39:09 -0700

    got the retriever working

--- app/generate_response.py
+++ app/generate_response.py
@@ -7,25 +7,39 @@ from langchain.embeddings import OpenAIEmbeddings
 
 load_dotenv()
 
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
 
 
-def generate_assistant_response(prompt, memory):
+def format_docs(docs):
+    return "\n\n".join([d.page_content for d in docs])
+
+def generate_assistant_response(query, memory):
     model = ChatOpenAI(model = 'gpt-4-1106-preview')
     embedding_function = OpenAIEmbeddings()
     db = Chroma(
-        persist_directory="./11-Langchain-Bot/langchain_documents_db",
+        persist_directory="./db_chemo_guide",
         embedding_function=embedding_function,
     )
+    # print(db.get_document_count())
 
-    retriever = db.as_retriever(search_type="mmr")
+    retriever = db.as_retriever(search_type="mmr")  
+    return retriever.get_relevant_documents(query, k=3)
+    # template = """Answer the question based only on the following context:
 
-    qa = RetrievalQA.from_chain_type(
-        llm=model,
-        chain_type="stuff",
-        retriever=retriever,
-        memory=memory,
-    )
-    response = qa(prompt)
+    # {context}
 
-    return response["result"]
+    # Question: {question}
+    # """
+    # prompt = ChatPromptTemplate.from_template(template)
+    # model = ChatOpenAI()
 
+    # chain = (
+    #     {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    #     | prompt
+    #     | model
+    #     | StrOutputParser()
+    # )
+    # return chain.invoke(query)
