f1a94875f02d4ce2cab399bb40500fc9c9e3a7f3
Jacob Coccari <jacob@Jacobs-MacBook-Pro.local>
2023-11-13 18:32:14 -0700

    got moderationchain to work

--- app/generate_response.py
+++ app/generate_response.py
@@ -2,9 +2,6 @@ from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 
-
-
-
 load_dotenv()
 
 from langchain.chat_models import ChatOpenAI
@@ -13,6 +10,7 @@ from langchain.schema.runnable import RunnablePassthrough
 
 from utls.compose_prompt import create_prompt
 from utls.format_memory import get_chat_history
+from utls.moderation import harmful_content_check
 
 
 def format_docs(docs):
@@ -21,9 +19,8 @@ def format_docs(docs):
 def generate_assistant_response(query, retriever, streamlit_memory):
     history = get_chat_history(streamlit_memory)
     prompt = create_prompt(history)
-    print(prompt)
     model = ChatOpenAI(model = 'gpt-4-1106-preview')
-    
+    # harmful_content_check(query)
     chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
         | prompt
